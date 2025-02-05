fc46e0e602ff1b77238fd1fe55746549c9f27f47
vctrklndr <victor@nasa.se>
2025-02-05 10:50:13 +0100

    Pappa Otto <3

---
+++ main.js
@@ -0,0 +1,27 @@
+let countdownInterval = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+  const targetDate = new Date("2025-09-01T09:00:00").getTime();
+  const now = new Date().getTime();
+  const timeLeft = targetDate - now;
+
+  if (timeLeft <= 0) {
+    document.getElementById("title").innerHTML = "Pappa Otto är tillbaka!";
+    document.getElementById("pappa-otto").classList.add("rotate-center");
+    clearInterval(countdownInterval);
+    return;
+  }
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById(
+    "countdown"
+  ).innerHTML = `${days} dagar, ${hours} timmar, ${minutes} minuter, ${seconds} sekunder`;
+}
+
+updateCountdown();
